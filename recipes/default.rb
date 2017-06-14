b5d54b189a9da00948290269cb02da2a43cc0230
anthonygrees <anthony_rees01@hotmail.com>
2017-06-14 11:20:12 +0000

    added group and tomcat file extract

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,5 +7,45 @@
 #
 #Install java on Centos
 yum_package 'java-1.7.0-openjdk-devel' do
-  action :install # Install is default and does not need to be specified
+  action :install # Install is default and does not need to be specified end group 'chef' group 'tomcat'
+#
+#Create group and user Chef
+group 'chef' 
+
+user 'chef' do
+  group 'chef'
+  system true
+  shell '/bin/bash'
 end
+#
+#Get the apache tomcat tar and put it in the tmp dir
+#
+remote_file '/tmp/apache-tomcat-8.5.15.tar.gz' do
+  source 'http://apache.uberglobalmirror.com/tomcat/tomcat-8/v8.5.15/bin/apache-tomcat-8.5.15.tar.gz'
+  owner 'chef'
+  group 'chef'
+  mode '0755'
+  action :create
+end
+#
+#Extract tomcat tar
+#
+directory "/opt/tomcat" do
+  group 'tomcat' 
+end 
+
+bash 'extract_module' do
+  cwd '/tmp'
+  code <<-EOH
+#    mkdir -p /opt/tomcat
+    tar xvf apache-tomcat-8.5.15.tar.gz -C /opt/tomcat --strip-components=1
+  EOH
+end
+
+directory '/opt/tomcat/conf' do 
+  mode '0070'
+end
+
+execute 'chmod g+r conf/*'
+execute 'chown -R tomcat webapps/ work/ temp/ logs/'
+
