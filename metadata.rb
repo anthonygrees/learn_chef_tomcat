22082bfa04eb79f80258c27b95deb9fd313285c8
anthonygrees <anthony_rees01@hotmail.com>
2017-06-14 01:23:33 +0000

    commit cookbook

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'learn_chef_tomcat'
+maintainer 'Anthony Rees'
+maintainer_email 'anthony_rees01@hotmail.com'
+license 'all_rights'
+description 'Installs/Configures learn_chef_tomcat'
+long_description 'Installs/Configures Tomcat on a Centos server'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/learn_chef_tomcat/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/learn_chef_tomcat' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook Name:: learn_chef_tomcat
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
