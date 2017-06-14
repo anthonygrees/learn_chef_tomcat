9731a90601a4647985895c10a60aa32574789f3c
anthonygrees <anthony_rees01@hotmail.com>
2017-06-14 11:16:43 +0000

    added more unit tests

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -10,3 +10,35 @@
 describe package 'java-1.7.0-openjdk-devel' do
   it { should be_installed }
 end
+
+#
+#Check the tomcat user and group
+describe group('tomcat') do
+  it { should exist }
+end
+
+describe user('tomcat' do
+  it { should exist }
+  it { should belong_to_group 'tomcat'}
+end
+
+#Check the tomcat service is enabled and running
+#
+describe service 'tomcat' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+#Check that Tomcat is up on localhost
+#
+describe 'tomcat::default' do
+  describe command("curl http://localhost:8080") do
+    its(:stdout) { should match /Tomcat/ }
+  end
+end
+
+#Check the Port 8080
+#
+describe port 8080 do
+  it { should be_listening }
+end
