42d5a436588be1ce70e6f1613f7b5d770307df01
anthonygrees <anthony_rees01@hotmail.com>
2017-06-14 01:45:36 +0000

    add java install to recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,3 +4,8 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
+#
+#Install java on Centos
+yum_package 'java-1.7.0-openjdk-devel' do
+  action :install # Install is default and does not need to be specified
+end
